17b7faa99836e7f4fb11386fe0ddba90c479a7a7
Natnael Badigo <mentalet@gmail.com>
2026-02-27 10:25:46 +0300

    feat: add initial React app setup with Vite and ESLint configuration

---
+++ todo_list/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      hello world, this is my first react project, and i am so excited to learn it!
+    </div>
+  )
+}
+
+export default App
