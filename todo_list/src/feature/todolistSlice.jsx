8a356b4b704c4027043f0e7083ea7685925527bc
Olyaad60 <olyaadw@gmail.com>
2026-02-27 08:00:53 -0500

    feat: added slice and api

---
+++ todo_list/src/feature/todolistSlice.jsx
@@ -0,0 +1,35 @@
+import {CreateSlice , createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import api from '../api/baseUrl';
+
+ const Fetchtodo=createAsyncThunk(
+    "fetchtodo/gettodo", async () =>{
+        const response = await api.get("/todo");
+        return response.data;
+    }
+ )
+const intialstate={
+    todo:[],
+    loading:false,
+    error:null
+}
+const todolistSlice=createSlice({
+    name:"todolist",
+    initialState:intialstate,
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(Fetchtodo.pending,(state)=>{
+            state.loading=true;
+            state.error=null;
+        })
+        .addCase(Fetchtodo.fulfilled,(state,action)=>{
+            state.loading=false;
+            state.todo=action.payload;
+        })
+        .addCase(Fetchtodo.rejected,(state,action)=>{
+            state.loading=false;
+            state.error=action.error.message;
+        })
+    }
+})
+export default todolistSlice.reducer;
