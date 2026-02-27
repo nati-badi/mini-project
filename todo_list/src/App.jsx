12fe543b89c02430cf9e422cf8e0941765ec288b
Natnael Badigo <mentalet@gmail.com>
2026-02-27 10:59:22 +0300

    feat: add tailwind css support

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='text-xl font-bold text-center mt-10'>
       hello world!
     </div>
   )
