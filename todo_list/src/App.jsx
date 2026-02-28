96888ec2cfe0fab4bfd1ea13bcd99ed246231ee1
olyaad60 <olyaadw@gmail.com>
2026-02-28 10:44:37 +0300

    fix:data and some feature

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -1,10 +1,26 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchTodo } from "./feature/todolistSlice";
 
 function App() {
   const dispatch = useDispatch();
-  const todo = useSelector((state) => state.todos.todo);
 
-  return <div className="text-xl font-bold text-center mt-10">{todo}</div>;
+  const { todos, loading, error } = useSelector((state) => state.todos);
+
+  useEffect(() => {
+    dispatch(fetchTodo());
+  }, [dispatch]);
+
+  return (
+    <div className="text-xl font-bold text-center mt-10">
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      {todos.map((todo) => (
+        <p key={todo.id}>{todo.name}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- todo_list/src/feature/todolistSlice.jsx
+++ todo_list/src/feature/todolistSlice.jsx
@@ -1,35 +1,39 @@
-import {CreateSlice , createAsyncThunk, createSlice} from "@reduxjs/toolkit"
-import api from '../api/baseUrl';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../api/baseUrl";
 
- const Fetchtodo=createAsyncThunk(
-    "fetchtodo/gettodo", async () =>{
-        const response = await api.get("/users");
-        return response.data;
-    }
- )
-const intialstate={
-    todo:[],
-    loading:false,
-    error:null
-}
-const todolistSlice=createSlice({
-    name:"todolist",
-    initialState:intialstate,
-    reducers:{},
-    extraReducers:(builder)=>{
-        builder
-        .addCase(Fetchtodo.pending,(state)=>{
-            state.loading=true;
-            state.error=null;
-        })
-        .addCase(Fetchtodo.fulfilled,(state,action)=>{
-            state.loading=false;
-            state.todo=action.payload;
-        })
-        .addCase(Fetchtodo.rejected,(state,action)=>{
-            state.loading=false;
-            state.error=action.error.message;
-        })
-    }
-})
+export const fetchTodo = createAsyncThunk(
+  "todolist/fetchTodo",
+  async () => {
+    const response = await api.get("/users");
+    return response.data;
+  }
+);
+
+const initialState = {
+  todos: [],
+  loading: false,
+  error: null,
+};
+
+const todolistSlice = createSlice({
+  name: "todolist",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodo.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTodo.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload;
+      })
+      .addCase(fetchTodo.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
 export default todolistSlice.reducer;
